b47383c6002c0d51498c29300c093091daeb889b
Felipe Franco <felipe@fabricadecodigo.com>
2021-09-14 20:08:18 -0300

    criação do projeto

---
+++ src/LinkManager.Api.GraphQL/Startup.cs
@@ -0,0 +1,70 @@
+using HotChocolate.Types;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace LinkManager.Api.GraphQL
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                // .AddAutoMapper(typeof(BusinessRules.BusinessRuleRequest))
+                .AddAuthorization()
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // Salva na API que o usuário está autenticado
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = Configuration.GetValue<string>("JWT:Audience"),
+                        ValidIssuer = Configuration.GetValue<string>("JWT:Issuer"),
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JWT:SecretKey"))),
+                        ValidateIssuer = true, // por padrão é true
+                        ValidateAudience = true, // por padrão é true
+                    };
+                });
+
+                services
+                    .AddGraphQLServer()
+                    .AddAuthorization()
+                    // ajustando o formatodo tipo Guid para 00000000-0000-0000-0000-000000000000
+                    // https://chillicream.com/docs/hotchocolate/defining-a-schema/scalars/#net-scalars
+                    .AddType(new UuidType('D'));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapGraphQL();
+                });
+        }
+    }
+}
